5b194e14f2706c97165bb7daf3b2dac208b5c7a7
Benjamin Benais <benjamin.benais@targetspot.com>
2020-03-09 14:01:01 +0100

    Calcul logic simplified

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,7 +12,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data() {
     return {
       x: 0,
@@ -27,55 +27,21 @@ export default {
       // Card measurements
       const cardHeight = e.target.clientHeight;
       const cardWidth = e.target.clientWidth;
-      // Half measurements
-      const middleY = cardHeight / 2;
-      const middleX = cardWidth / 2;
 
-      // Example for offsetY < middleY (top half of the card):
-      // `a` is the position of the mouse relative to top half the card, in percentage
-      // If the mouse is at the
-      // top of the card, `a` = 0;
-      // middle of the card, `a` = 100;
+      // Rotation values
+      const rotateX = this.getRotateValue(offsetY, cardHeight);
+      const rotateY = this.getRotateValue(offsetX, cardWidth);
 
-      // The value of `rotate` is between 10 and -10
-      // So we divide a by 10, and store the value in `b`
-      // In this case, we want to invert the percentage:
-      // The mouse at the top half gives us 10 and the bottom 0
-
-      this.scale = 1.07;
-
-      // TOP
-      if (offsetY < middleY) {
-        const a = (offsetY * 100) / middleY;
-        const b = a / 10;
-        this.x = b - 10;
-      }
-
-      // BOTTOM
-      if (offsetY > middleY) {
-        const a = (offsetY * 100) / middleY;
-        const b = a / 10;
-        this.x = b - 10;
-      }
-
-      // LEFT
-      if (offsetX < middleX) {
-        const a = (offsetX * 100) / middleX;
-        const b = a / 10;
-        this.y = 10 - b;
-      }
-
-      // RIGHT
-      if (offsetX > middleX) {
-        const a = (offsetX * 100) / middleX;
-        const b = (a / 10) * -1;
-        this.y = b + 10;
-      }
+      this.x = rotateX;
+      this.y = rotateY;
+    },
+    getRotateValue(offset, length) {
+      const middle = length / 2;
+      return (offset * 100) / middle / 10 - 10;
     },
     handleMouseLeave() {
       this.x = 0;
       this.y = 0;
-      this.scale = 1;
     }
   },
   computed: {
